46f6bd7eceb4820e8e146d6c3ffac07044c5f4db
wonee <hwang9821@icloud.com>
2022-05-16 14:48:33 +0900

    feat : 예약, 조회, 삭제 기능

--- reservation/src/main/java/com/sw/reservation/core/CoreController.java
+++ reservation/src/main/java/com/sw/reservation/core/CoreController.java
@@ -1,14 +1,15 @@
 package com.sw.reservation.core;
 
-import com.sw.reservation.core.request.PostCoreReq;
-import com.sw.reservation.room.Room;
-import com.sw.reservation.users.User;
+import com.sw.reservation.common.errors.NotFoundException;
+import com.sw.reservation.common.utils.ApiUtils;
+import com.sw.reservation.common.utils.ApiUtils.ApiResult;
+import com.sw.reservation.core.request.PostReservationReq;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.sw.reservation.common.utils.ApiUtils.success;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,11 +17,39 @@ import org.springframework.web.bind.annotation.RestController;
 public class CoreController {
     private final CoreService coreService;
 
-    @PostMapping()
-    public ResponseEntity<Core> createCore(@RequestBody CoreDto coreDto){
+    /**
+     * 예약 하기
+     * [post] /reservation
+     * Body
+     * {
+     *     "startTime":"2022-01-12T14:00",
+     *     "endTime":"2022-01-12T14:30",
+     *     "roomId":1,
+     *     "studentId":1923717
+     * }
+     */
+    @PostMapping
+    public ApiResult<String> createCore(@RequestBody PostReservationReq postReservationReq){
+        return success(coreService.createCore(postReservationReq));
+    }
 
-        return ResponseEntity.status(200).body(
-                coreService.createCore()
-        );
+    // 강의실 별 예약 현황
+    @GetMapping("/{roomId}")
+    public List<Core> getCore(@PathVariable Long roomId){
+        return coreService.getByCore(roomId);
+    }
+
+    // 유저 별 예약 현황
+    @GetMapping("/users/{studentId}")
+    public ApiResult<Core> getUserCore(@PathVariable Long studentId){
+        return success(coreService.getById(studentId)
+                .orElseThrow(() -> new NotFoundException("exception")));
+    }
+
+    // 예약한 강의실 삭제
+    @DeleteMapping("/{id}")
+    public ApiResult<String> deleteCore(@PathVariable Long id){
+        coreService.deleteReservation(id);
+        return ApiUtils.success("예약을 삭제했습니다.");
     }
 }
