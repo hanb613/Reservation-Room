39b7474b948acaabf2af3804dd6f3d8a585b0813
wonee <hwang9821@icloud.com>
2022-05-29 00:31:04 +0900

    test: 백엔드 배포 오류 test

--- reservation/src/test/java/com/sw/reservation/ReservationApplicationTests.java
+++ reservation/src/test/java/com/sw/reservation/ReservationApplicationTests.java
@@ -3,7 +3,7 @@ package com.sw.reservation;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ReservationApplicationTests {
 
     @Test
